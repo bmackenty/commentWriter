5c8e2378600f70eaf6c31aa8c38f5fdda03c2655
Bill MacKenty <bmackenty@users.noreply.github.com>
2023-01-17 16:03:54 +0100

    Create templates.py

---
+++ templates.py
@@ -0,0 +1,22 @@
+
+
+
+comments = {
+    9: '''
+    NAME is beginning to design solutions through programming. THIRD_PERSON_SINGULAR_PRONOUN is designing and constructing an application to PROJECT_DESCRIPTION.  
+    This is a classic first-year-project, where THIRD_PERSON_SINGULAR_PRONOUN will need to store attributes for different PROJECT_NOUN in a 
+    database, and then retrieve PROJECT_NOUN based on user input.   The Design cycle is composed of four interconnected parts; inquiry, planning, 
+    construction and evaluation.  NAME has demonstrated excellent skill and understanding in the first two parts. THIRD_PERSON_SINGULAR_PRONOUN is learning and applying the PHP programming language 
+    PROGRAMMING_SKILL. When asked what THIRD_PERSON_SINGULAR_PRONOUN wanted to learn, THIRD_PERSON_SINGULAR_PRONOUN replied “STUDENT_VOICE”. POSSESIVE_PRONOUN reply evidenced a desire to become
+    more independent. THIRD_PERSON_SINGULAR_PRONOUN will be given this opportunity in class during our second design project. 
+    NAME can also independently work on projects and be comfortable "not knowing". NAME produces excellent-quality work. THIRD_PERSON_SINGULAR_PRONOUN communicates basic understanding of 
+    most concepts and contexts with few misunderstandings and minor gaps. NAME is CLASSROOM_BEHAVIOR. 
+    NAME often demonstrates basic critical and creative thinking.  THIRD_PERSON_SINGULAR_PRONOUN uses knowledge and skills with some 
+    flexibility in familiar classroom situations, but requires support in unfamiliar situations. NAME can 
+    improve GENDER_THIRD_PERSON knowledge and understanding by asking for help more often, independently attempting difficult 
+    coding projects and seeking ways to add complexity to GENDER_THIRD_PERSON projects. Designing solutions is incredibly 
+    challenging and rewarding.  NAME should continue to push THIRD_PERSON_PLURAL_GENDER to computationally solve problems by 
+    independently attempting to code, solving problem-sets, and computational thinking.'''
+
+
+}
