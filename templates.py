5f6145f4d110443c4a3e710a59fa81a7c066f8d3
Bill MacKenty <bmackenty@users.noreply.github.com>
2023-01-18 16:04:35 +0100

    Update templates.py

--- templates.py
+++ templates.py
@@ -6,7 +6,7 @@ comments = {
     NAME is beginning to design solutions through programming. THIRD_PERSON_SINGULAR_PRONOUN is designing and constructing an application to PROJECT_DESCRIPTION.  
     This is a classic first-year-project, where THIRD_PERSON_SINGULAR_PRONOUN will need to store attributes for different PROJECT_NOUN in a 
     database, and then retrieve PROJECT_NOUN based on user input.   The Design cycle is composed of four interconnected parts; inquiry, planning, 
-    construction and evaluation.  NAME has demonstrated excellent skill and understanding in the first two parts. THIRD_PERSON_SINGULAR_PRONOUN is learning and applying the PHP programming language 
+    construction and evaluation.  NAME has demonstrated excellent skill and understanding in the first two parts. We are still working on the second two parts. THIRD_PERSON_SINGULAR_PRONOUN is learning and applying the PHP programming language 
     PROGRAMMING_SKILL. When asked what THIRD_PERSON_SINGULAR_PRONOUN wanted to learn, THIRD_PERSON_SINGULAR_PRONOUN replied “STUDENT_VOICE”. POSSESIVE_PRONOUN reply evidenced a desire to become
     more independent. THIRD_PERSON_SINGULAR_PRONOUN will be given this opportunity in class during our second design project. 
     NAME can also independently work on projects and be comfortable "not knowing". NAME produces excellent-quality work. THIRD_PERSON_SINGULAR_PRONOUN communicates basic understanding of 
@@ -15,8 +15,8 @@ comments = {
     flexibility in familiar classroom situations, but requires support in unfamiliar situations. NAME can 
     improve GENDER_THIRD_PERSON knowledge and understanding by asking for help more often, independently attempting difficult 
     coding projects and seeking ways to add complexity to GENDER_THIRD_PERSON projects. Designing solutions is incredibly 
-    challenging and rewarding.  NAME should continue to push THIRD_PERSON_PLURAL_GENDER to computationally solve problems by 
-    independently attempting to code, solving problem-sets, and computational thinking.'''
-
+    challenging and rewarding.  Designing solutions is incredibly challenging and rewarding. NAME should continue to push THIRD_PERSON_PLURAL_GENDER 
+    to computationally solve problems by independently attempting to code, solve problem-sets, and think computationally.
+'''
 
 }
